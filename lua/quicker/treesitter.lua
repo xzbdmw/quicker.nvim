c58148f0a190bf99efb45433fe9938664419cd2f
xzb <2598514867@qq.com>
2025-02-16 16:34:33 +0800

    zz

--- lua/quicker/treesitter.lua
+++ lua/quicker/treesitter.lua
@@ -93,6 +93,9 @@ function M._attach_lang(buf, lang, regions, register_cbs)
   --    which means it only parses visible areas (the on_win and on_line callback),
   --    so if we modify the buffer, unvisited area's state get unsynced.
   pcall(parser.parse, parser, true)
+  vim.api.nvim_buf_call(buf, function()
+    vim.cmd([[norm! "_xu]])
+  end)
 end
 
 return M
